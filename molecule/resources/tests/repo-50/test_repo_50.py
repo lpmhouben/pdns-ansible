965c0e6504ffbee5a74afa0a215bba520ef31f4c
Rens Houben <rhouben@systemec.nl>
2025-10-02 17:08:17 +0200

    Added 5.0, removed 4.7, apt repo to deb822 Removed ansible 2.14 because it no longer works with the images.

--- molecule/resources/tests/repo-50/test_repo_50.py
+++ molecule/resources/tests/repo-50/test_repo_50.py
@@ -6,9 +6,9 @@ rhel_os = ['redhat', 'centos', 'ol', 'rocky', 'almalinux']
 def test_repo_file(host):
     f = None
     if host.system_info.distribution.lower() in debian_os:
-        f = host.file('/etc/apt/sources.list.d/powerdns-auth-48.list')
+        f = host.file('/etc/apt/sources.list.d/powerdns-auth-50.sources')
     if host.system_info.distribution.lower() in rhel_os:
-        f = host.file('/etc/yum.repos.d/powerdns-auth-48.repo')
+        f = host.file('/etc/yum.repos.d/powerdns-auth-50.repo')
 
     assert f.exists
     assert f.user == 'root'
@@ -18,12 +18,12 @@ def test_repo_file(host):
 def test_pdns_repo(host):
     f = None
     if host.system_info.distribution.lower() in debian_os:
-        f = host.file('/etc/apt/sources.list.d/powerdns-auth-48.list')
+        f = host.file('/etc/apt/sources.list.d/powerdns-auth-50.sources')
     if host.system_info.distribution.lower() in rhel_os:
-        f = host.file('/etc/yum.repos.d/powerdns-auth-48.repo')
+        f = host.file('/etc/yum.repos.d/powerdns-auth-50.repo')
 
     assert f.exists
-    assert f.contains('auth-48')
+    assert f.contains('auth-50')
 
 
 def test_repo_pinning_file(host):
@@ -41,4 +41,4 @@ def test_pdns_version(host):
     cmd = host.run('/usr/sbin/pdns_server --version')
 
     assert 'PowerDNS Authoritative Server' in cmd.stderr
-    assert '4.8' in cmd.stderr
+    assert '4.9' in cmd.stderr
